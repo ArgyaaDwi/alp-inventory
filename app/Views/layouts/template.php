6ba794b49615c9f2021640806611e39942262be5
argyadwi <argyawoles@gmail.com>
2024-08-07 09:22:55 +0700

    Initial commit

--- app/Views/layouts/template.php
+++ app/Views/layouts/template.php
@@ -8,7 +8,7 @@
     <link rel="icon" type="image/jpg" href="images/logoalp.jpg">
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
+    <!-- Font Awesom -->
     <link rel="stylesheet" href="<?= base_url() ?>/plugins/fontawesome-free/css/all.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="<?= base_url() ?>/dist/css/adminlte.min.css">
